60f489d3681879e8d54c7daf16f570e10b99bb84
missahrizona <princessssahri@gmail.com>
2022-09-08 18:33:17 -0500

    str true

--- api/access.ts
+++ api/access.ts
@@ -1,7 +1,7 @@
 import type { VercelRequest, VercelResponse } from '@vercel/node';
 
 export default async (req: VercelRequest, res: VercelResponse) => {
-  res.setHeader('Access-Control-Allow-Credentials', true);
+  res.setHeader('Access-Control-Allow-Credentials', 'true');
   res.setHeader('Access-Control-Allow-Origin', '*');
   // another common pattern
   // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
